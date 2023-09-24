37819e85ac6f07ef9d34f65cf5a43bd07566131d
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-09-24 16:53:26 +0530

    📝 feat: hidden date paragraph updated

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -5,44 +5,38 @@ import Card from "./Card";
 import "./css/Expenses.css";
 
 function Expenses(props) {
+  // if else condition for hidden dates
+  const [hiddenDate, updateHiddenDate] = useState("(2020, 2021, 2022)");
 
-
-// if else condition for hidden dates
-const [hiddenDate, updateHiddenDate] = useState('(2020, 2021, 2022)')
-
-
-// Getting logs of dates on change
-  const [filterYear, setFilteredYear] = useState('2019');
+  // Getting logs of dates on change
+  const [filterYear, setFilteredYear] = useState("2019");
   const filterChangeHandler = (selectedYear) => {
     // console.log('Expenses.js');
     // console.log(selectedYear)
     setFilteredYear(selectedYear);
 
     // // if else condition for hidden dates
-    if(selectedYear === '2019'){
-      updateHiddenDate('(2020, 2021, 2022)')
-    }else if(selectedYear === '2020'){
-      updateHiddenDate('(2019, 2021, 2022)')
+    if (selectedYear === "2019") {
+      updateHiddenDate("(2020, 2021, 2022)");
+    } else if (selectedYear === "2020") {
+      updateHiddenDate("(2019, 2021, 2022)");
+    } else if (selectedYear === "2021") {
+      updateHiddenDate("(2019, 2020, 2022)");
+    } else {
+      updateHiddenDate("(2019, 2020, 2021)");
     }
-    else if(selectedYear === '2021'){
-      updateHiddenDate('(2019, 2020, 2022)')
-    }   else{
-      updateHiddenDate('(2019, 2020, 2021)')
-    }
-  }
-
-
-
-
-
+  };
 
   return (
     <Card className="expenses">
-      <ExpensesFilter onChangeFilter={filterChangeHandler} 
+      <ExpensesFilter
+        onChangeFilter={filterChangeHandler}
         defaultYear={filterYear}
       />
 
-<p><b>Dates:</b> {hiddenDate} are hidden</p>
+      <p>
+        <b>Dates:</b> for year {hiddenDate} are hidden
+      </p>
 
       <h2>Let's get started!</h2>
 
